7e34547e416083a3f2ee4fba87e1150da63bf462
Kryštof Woldřich <krystof51@gmail.com>
2018-11-03 14:04:20 +0200

    add empty mutation query, add dictionary typedef

--- server/src/graphql/data/typeDefs.js
+++ server/src/graphql/data/typeDefs.js
@@ -1,10 +1,15 @@
 import { gql } from "apollo-server-express";
 import { typeDef as User } from "./schema/user";
+import { typeDef as Dictionary } from "./schema/dictionary";
 
 const Query = gql`
   type Query {
     _empty: String
   }
+
+  type Mutation {
+    _empty: String
+  }
 `;
 
-export default [Query, User];
+export default [Query, User, Dictionary];
