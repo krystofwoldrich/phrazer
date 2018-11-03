c47c9272a9cb8b93d42089a34a06e36543fd8358
Kryštof Woldřich <krystof51@gmail.com>
2018-11-03 14:03:18 +0200

    add dictionaries resolvers

--- server/src/graphql/data/resolvers.js
+++ server/src/graphql/data/resolvers.js
@@ -1,6 +1,7 @@
 import { merge } from "lodash";
 import { resolvers as userResolvers } from "./schema/user";
+import { resolvers as dictionaryResolvers } from "./schema/dictionary";
 
 const resolvers = {};
 
-export default merge(resolvers, userResolvers);
+export default merge(resolvers, userResolvers, dictionaryResolvers);
