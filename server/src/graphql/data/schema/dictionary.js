680ab341005864ea1462293604a5f288c0495c67
Kryštof Woldřich <krystof51@gmail.com>
2018-11-03 14:05:42 +0200

    add createDictionary mutation

--- server/src/graphql/data/schema/dictionary.js
+++ server/src/graphql/data/schema/dictionary.js
@@ -1,39 +1,45 @@
 import { gql } from "apollo-server-express";
 import User from "./../../../model/PhUser";
+import Dictionary from "./../../../model/PhDictionary";
 
 export const typeDef = gql`
   type Dictionary {
     id: String!
     name: String
-    user: User!
-    phrases: [Phrase]
+    phrases: [String]
     nativeLanguage: String!
     foreignLanguage: String!
   }
   extend type Query {
     getDictionaries: [Dictionary]
   }
-  type Mutation {
+  extend type Mutation {
     createDictionary(
       name: String
-      user: User!
+      userId: String!
       nativeLanguage: String!
       foreignLanguage: String!
     ): Dictionary
-    updateDictionary(id: ID!, name: String, phrases: [Phrase]): Dictionary
+    updateDictionary(id: ID!, name: String, phrases: [String]): Dictionary
     deleteDictionary(id: ID!): Dictionary
   }
 `;
 
 export const resolvers = {
-  Query: {
-    getDictionaries: () => {
-      return null;
-    }
-  },
   Mutation: {
-    createDictionary: (obj, args, context, info) => {
-      return null;
+    createDictionary: async (obj, args, context, info) => {
+      const newDic = new Dictionary(args);
+
+      const user = await User.findById(args.userId)
+        .exec()
+        .then(user => user)
+        .catch(err => console.log(err));
+
+      user.dictionaries.push(newDic);
+
+      user.save();
+
+      return newDic;
     },
     updateDictionary: (obj, args, context, info) => {
       return null;
