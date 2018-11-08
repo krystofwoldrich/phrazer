3aa6a5f9a5532a0242ab10d3deefd3b2a396997e
Kryštof Woldřich <krystof51@gmail.com>
2018-11-09 00:08:52 +0200

    removed static test data

--- client/reducers/phrazeReducer.js
+++ client/reducers/phrazeReducer.js
@@ -1,72 +1,8 @@
 import * as actionTypes from "../actions/actionTypes";
 import Category1 from "./../content/vacabulary/maa_ja_pohjarakennus.json";
-
-const initState2 = {
-  phrazes: [
-    {
-      key: "1",
-      library: "finnish",
-      category: "Greeting",
-      phraze: "How are you doing?",
-      translated: "Mitä kuuluu?",
-      public: false,
-      phrazed: false,
-      favorite: true
-    },
-    {
-      key: "2",
-      library: "finnish",
-      category: "Greeting",
-      phraze: "Where are you from?",
-      translated: "Mistä olet kotoisin?",
-      public: false,
-      phrazed: true,
-      favorite: true
-    },
-    {
-      key: "3",
-      library: "finnish",
-      category: "Greeting",
-      phraze: "What is your name?",
-      translated: "Mikä on sinun nimesi?",
-      public: false,
-      phrazed: false,
-      favorite: false
-    },
-    {
-      key: "4",
-      library: "finnish",
-      category: "Meeting",
-      phraze: "Do you speak english?",
-      translated: "Puhutko englantia?",
-      public: false,
-      phrazed: false,
-      favorite: false
-    },
-    {
-      key: "5",
-      library: "finnish",
-      category: "Meeting",
-      phraze: "It was nice to meet you",
-      translated: "Oli kiva tavata",
-      public: false,
-      phrazed: true,
-      favorite: true
-    },
-    {
-      key: "6",
-      library: "finnish",
-      category: "Hangout",
-      phraze: "You are welcome",
-      translated: "Ole hyvä",
-      public: false,
-      phrazed: true,
-      favorite: false
-    }
-  ],
-  phrazesByCategory: [],
-  selectedCategory: ""
-};
+import Category2 from "./../content/vacabulary/perustukset.json";
+import Category3 from "./../content/vacabulary/runko_ja_vesikattorakenteet.json";
+import Category4 from "./../content/vacabulary/tital.json";
 
 const initState = {
   phrazes: [],
